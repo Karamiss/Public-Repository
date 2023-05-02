8a596dd391f60f1af1d8e45296042f0e14ab72c0
Karamiss <Lisrise@yandex.ru>
2023-05-02 13:05:28 +0300

    Heilung
    Website of the music group

---
+++ Heilung/script.js
@@ -0,0 +1,10 @@
+<body>
+<script scr="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js" ></script>
+  <script>
+    function slowScroll(id) {
+    $('html, body').animate({
+    scrollTop: $(id).offset().top
+    }, 500)
+  }
+  </script>
+</body>
